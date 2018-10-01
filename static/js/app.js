4bf70fbb688f208f53c9efa63f6cb31644105832
Fiona Sterling <fsterling@hchc.edu>
2018-10-01 13:42:01 -0400

    Remove product from cart working correctly, TODO: don't show cart if cart is empty.

--- static/js/app.js
+++ static/js/app.js
@@ -65,7 +65,7 @@ function addToCart(id) {
   sleep(50).then(() => {
       // Do something after the sleep!
       showCart();
-  })
+  });
 
 }
 
@@ -83,6 +83,7 @@ function showCart() {
       <td>${cart[i].title}</td>
       <td>$${cart[i].price}</td>
       <td>$${cart[i].price}</td>
+      <td><button onclick="removeFromCart(${cart[i].id})" class="btn btn-danger">x</button></td>
     </tr>
     `;
   }
@@ -91,6 +92,28 @@ function showCart() {
   // inject html variable into table-body
   $("#table-body").html(html);
 }
+
+
+// TODO: create remove functionality
+function removeFromCart(id) {
+  // loop through products in cart and remove one instance of id
+  for (let i = 0; i < cart.length; i++) {
+    // check current product for id passed in
+    if (cart[i].id == id) {
+      // splice current product
+      cart.splice(i, 1);
+      break;
+    }
+  }
+
+  // run showCart to generate current cart array
+  // Usage!
+  sleep(50).then(() => {
+      // Do something after the sleep!
+      showCart();
+  });
+}
+
 
 // https://zeit.co/blog/async-and-await
 function sleep (time) {
